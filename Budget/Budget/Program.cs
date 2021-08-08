c2b707d01bb76bfb40f7a5f12bf9331d6f12f1cf
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-08 11:36:55 +1000

    Add some messages to display on the console and commit some small config changes

--- Budget/Budget/Program.cs
+++ Budget/Budget/Program.cs
@@ -6,7 +6,15 @@ namespace Budget
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("####### You are now using Budgeting ######");
+            Console.WriteLine("******* Press any key to continue *******");
+            Console.ReadLine();
+            Console.WriteLine("Please enter in your total weekly expenses ");
+            int expense = Int32.Parse(Console.ReadLine());
+            
+            //Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
         }
     }
 }
