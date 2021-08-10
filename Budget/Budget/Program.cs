7745bb619a6cf3333f365843372541e0a7a40d6e
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-10 17:10:41 +1000

    Add some more messages to be displayed in the console and add some additional small config changes

--- Budget/Budget/Program.cs
+++ Budget/Budget/Program.cs
@@ -11,9 +11,21 @@ namespace Budget
             Console.WriteLine("###################################################\n");
             Console.WriteLine("**********Press any key to continue****************\n");
             Console.ReadLine();
+            
             Console.Write("Please enter in your total weekly expenses: ");
             int expense = Int32.Parse(Console.ReadLine());
-            
+
+            Console.Write("\n");
+
+            Console.Write("Please enter in your age: ");
+            string age = Console.ReadLine();
+
+            Console.Write("\n");
+
+            Console.WriteLine("//////Calculation process complete. Press any key to see your result//////");
+
+
+
             //Console.WriteLine("Hello World!");
 
             Console.ReadLine();
